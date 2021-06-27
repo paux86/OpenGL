b74dba5bc0a7300c8cd991f18640926d68702427
Matt Brierley <matt.brierley86@gmail.com>
2021-06-27 14:27:06 -0700

    Added 2D dynamic batch rendering

--- OpenGL_3D/res/shaders/Basic.shader
+++ OpenGL_3D/res/shaders/Basic.shader
@@ -1,31 +1,39 @@
 #shader vertex
-#version 330 core
+#version 450 core
 
-layout(location = 0) in vec4 position;
-layout(location = 1) in vec2 texCoord;
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec4 a_Color;
+layout(location = 2) in vec2 a_TexCoord;
+layout(location = 3) in float a_TexIndex;
 
+uniform mat4 u_ViewProj;
+uniform mat4 u_Transform;
+
+out vec4 v_Color;
 out vec2 v_TexCoord;
-
-uniform mat4 u_MVP;
+out float v_TexIndex;
 
 void main()
 {
-	gl_Position = u_MVP * position;
-	v_TexCoord = texCoord;
+	v_Color = a_Color;
+	v_TexCoord = a_TexCoord;
+	v_TexIndex = a_TexIndex;
+	gl_Position = u_ViewProj * u_Transform * vec4(a_Position, 1.0);
 };
 
 #shader fragment
-#version 330 core
+#version 450 core
 
-layout(location = 0) out vec4 color;
+layout(location = 0) out vec4 o_Color;
 
+in vec4 v_Color;
 in vec2 v_TexCoord;
+in float v_TexIndex;
 
-uniform vec4 u_Color;
-uniform sampler2D u_Texture;
+uniform sampler2D u_Textures[32];
 
 void main()
 {
-	vec4 texColor = texture(u_Texture, v_TexCoord);
-	color = texColor;
+	int index = int(v_TexIndex);
+	o_Color = texture(u_Textures[index], v_TexCoord) * v_Color;
 };
--- OpenGL_3D/src/Application.cpp
+++ OpenGL_3D/src/Application.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+#include "Renderer.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -11,12 +18,12 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "OpenGL Stuff", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "OpenGL Stuff", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -32,20 +39,70 @@ int main(void)
 		std::cout << "Error!" << std::endl;
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		Shader shader("res/shaders/Basic.shader");
 
-		/* Poll for and process events */
-		glfwPollEvents();
+		shader.Bind();
+		int samplers[32];
+		for (int i = 0; i < 32; i++)
+			samplers[i] = i;
+		shader.SetUniform1iv("u_Textures", 32, samplers);
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+		Renderer::Init();
+
+		GLuint texture1 = Texture("res/textures/hero_dash_icon.png").GetId();
+		GLuint texture2 = Texture("res/textures/shield_with_cross_icon.png").GetId();
+
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			glClear(GL_COLOR_BUFFER_BIT);
+			shader.Bind();
+			glm::mat4 viewProj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f) * glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+			glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+			shader.SetUniformMat4f("u_ViewProj", viewProj);
+			shader.SetUniformMat4f("u_Transform", transform);
+
+			Renderer::ResetStats();
+			Renderer::BeginBatch();
+
+			for (float y = -10.f; y < 10.0f; y += 0.25f)
+			{
+				for (float x = -10.f; x < 10.0f; x += 0.25f)
+				{
+					glm::vec4 color = { (x + 10) / 20.0f, 0.2f, (y + 10) / 20.0f, 1.0f };
+					Renderer::DrawQuad({ x, y }, { 0.25f, 0.25f }, color);
+				}
+			}
+
+			for (int y = 0; y < 5; y++)
+			{
+				for (int x = 0; x < 5; x++)
+				{
+					GLuint tex = (x + y) % 2 == 0 ? texture1 : texture2;
+					Renderer::DrawQuad({ x, y }, { 1.0f, 1.0f }, tex);
+				}
+			}
+
+			Renderer::EndBatch();
+
+			Renderer::Flush();
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+		
+		Renderer::Shutdown();
 	}
-
 	glfwTerminate();
 	return 0;
 }
